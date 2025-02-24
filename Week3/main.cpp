42e57e02c59cb1985f1012b73fea9424dbf811d2
ans-xh69 <pokeytree567@gmail.com>
2025-02-24 16:35:23 +0500

    Tried to make a few changes

---
+++ Week3/main.cpp
@@ -0,0 +1,106 @@
+		/* Example 3.2	*/
+
+//#include<iostream>
+//#include<conio.h>
+//
+//using namespace std;
+//
+//void rev()
+//{
+//	char ch;
+//	cin.get(ch);
+//	if (ch != '\n') {
+//		rev();
+//		cout.put(ch);
+//	}
+//}
+//int main() {
+//	rev();
+//	_getch();
+//	return 0;
+//}
+
+		/* Exercise 3.1	*/
+
+//#include<iostream>
+//using namespace std;
+//
+//int Power(int X, int N)
+//{
+//	if (N == 0)
+//		return 1;
+//	else
+//		return(Power(X, N - 1) * X);
+//}
+//int main()
+//{
+//	int a = 2;
+//	for (int i = 0; i <= 20; i++) {
+//		cout << a << " ^ " << i << " = " << Power(a, i) << endl;
+//	}
+//}
+
+		/* Exercise 3.2	*/
+
+//#include<iostream>
+//using namespace std;
+//
+//int Ackermann(int m, int n) {
+//	if (m == 0)
+//		return n + 1;
+//
+//	else if (m > 0 && n == 0)
+//		return Ackermann(m - 1, 1);
+//
+//	else if (m > 0 && n > 0)
+//		return Ackermann(m - 1, Ackermann(m, n - 1));
+//}
+//
+//int main() 
+//{
+//	cout << Ackermann(3, 4) << endl;
+//	return 0;
+//}
+
+		/* Exercise 3.3	*/
+
+        #include<iostream>
+        using namespace std;
+        
+        //int print(int x)
+        //{
+        //	if (x < 0)
+        //	{
+        //		return 1;
+        //	}
+        //	cout << x << " ";
+        //	print(x - 1);
+        //}
+        
+        int Binomial(int a, int b) {
+            if (a == b || b==0) {
+                return 1;
+            }
+                return(Binomial(a - 1, b - 1) + Binomial(a - 1, b));
+        }
+        
+        int main()
+        {
+            /*int a = 9;
+            print(a);*/
+            int x = 0, int y = 0;
+        
+            cout << "k: ";
+            cin >> x;
+            cout << "n: ";
+            cin >> y;
+        
+            if (x < 0 || x > y) {
+                cout << "Error!\n";
+                return 1;
+            }
+        
+            cout << endl << Binomial(x,y);
+        
+            return 0;
+        }
